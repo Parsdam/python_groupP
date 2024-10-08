7e7cc7644043847079829eff658a5f867794841a
Parsdam <cout.name.hosin01001@gmail.com>
2024-10-08 20:01:07 +0330

    endd the first type

--- hosintesting.py
+++ hosintesting.py
@@ -1,11 +1,18 @@
 import random
 score = 0
 def playgame(min_number, max_number, max_tries):
+    attempt = 0
     number_to_guess = random.randint(min_number, max_number)
     print(f"I'm thinking of a number between {min_number} and {max_number}. You have {max_tries} tries.")
-    attempt = 0
+    
     for attempt in range(max_tries - 1):
-        guess = int(input("whats youre guess ? "))
+        try:
+            guess = int(input("whats youre guess ? "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            attempt -= 1
+            continue
+
         if guess == number_to_guess:
             print(f"Congratulations! You guessed the number {number_to_guess} in {attempt+1} attempts.")
             score += 1
@@ -15,6 +22,7 @@ def playgame(min_number, max_number, max_tries):
 
         elif guess > number_to_guess:
             print("Too high!")
+        
         elif guess > max_number or guess < min_number :
             print("Invalid input. Please enter a number.") 
             attempt -= 1
@@ -23,7 +31,7 @@ def main():
 
     print("welcome to my gussing game! \n ------------------------------------------------------------")
 
-    difficulty_lvl = int(input("Choose your difficulty level (you will have 4 chancess to guess correctly): \n1. Easy (1-10) \n2. Medium (1-100) \n3. Hard (1-1000)"))
+    difficulty_lvl = int(input("Choose your difficulty level (you will have 4 chancess to guess correctly): \n1. Easy (1-10) \n2. Medium (1-100) \n3. Hard (1-1000) \n ??? "))
     
     match (difficulty_lvl):
         case (1):
