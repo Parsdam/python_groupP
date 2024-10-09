e6da1835df430f141d426148f520743c2c5d9e8b
Parsdam <cout.name.hosin01001@gmail.com>
2024-10-09 22:30:30 +0330

    Fixed bug in gusscharecter function: corrected list append and removed unnecessary chr function

--- hossin_wordguessing.py
+++ hossin_wordguessing.py
@@ -1,8 +1,27 @@
 import random
-def gusscharecter():
-    print("you got it right")
+def gusscharecter(lenght,word):
+    attempts = 10
+    print("ok now you should guess a charecter and im ganna tell you if the word has it\nthere are :")
+    print(lenght * "_ ")
+    found_charecterlist = []
+    while attempts > 0:
+        try:
+            guess = chr(input("guess a charecter: ").lower())
+            if guess in word and not(guess in found_charecterlist):
+                print(f"yes the word has the charecter {guess}")
+                found_charecterlist = found_charecterlist.append(guess)
+                print(f"u have found so far is :{found_charecterlist}")
+            else:
+                print(f"no the word does not have the charecter {guess} or u have found this one before")
+                print(f"u have found so far is :{found_charecterlist}")
+                print(f"you have {attempts - 1}  attemts remaining")
+                
+        except ValueError:
+            print("Invalid input. Please enter a charecter.")
+            attempts -= 1
+
 def guess_length(length):
-    attempts =4
+    attempts = 4
     print(f"ok first you need to guess the length of the word.\nyou have {attempts} attempts to guess")
 
     while attempts > 0:
@@ -36,7 +55,7 @@ def main():
         print("im thinking of a word ....")
         word = randomword()
         if guess_length(len(word)):
-            gusscharecter()
+            gusscharecter(len(word),word)
 
 if __name__ == "__main__":
     main()
