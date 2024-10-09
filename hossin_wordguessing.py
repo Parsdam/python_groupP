5ed1bf0a4933e963da000dd0a7747e0188e5eb12
Parsdam <cout.name.hosin01001@gmail.com>
2024-10-09 23:08:07 +0330

    add a counter for the found_charecterlist and a way for the player to see how many of each charecter there are

--- hossin_wordguessing.py
+++ hossin_wordguessing.py
@@ -1,20 +1,21 @@
 import random
 def gusscharecter(lenght,word):
     attempts = 10
-    print("ok now you should guess a charecter and im ganna tell you if the word has it\nthere are :")
-    print(lenght * "_ ")
+    print("ok now you should guess a charecters in the word and im ganna tell you if the word has it or not\nthere are :")
+    print(lenght * "_ " + " charecters")
     found_charecterlist = []
     while attempts > 0:
         try:
             guess = input("guess a charecter: ").lower()
             if guess in word and not(guess in found_charecterlist):
-                print(f"yes the word has the charecter {guess}")
-                found_charecterlist.append(guess)
+                print(f"yes the word has {word.count(guess)} of the charecter {guess}")
+                found_charecterlist.append((word.count(guess),guess))
                 print(f"u have found so far is :{found_charecterlist}")
                 attempts -= 1
+                print(f"you have {attempts}  attemts remaining")
             else:
                 print(f"no the word does not have the charecter {guess} or u have found this one before")
-                print(f"u have found so far is :{found_charecterlist}")
+                print(f"what u have found so far is :{found_charecterlist}")
                 print(f"you have {attempts - 1}  attemts remaining")
                 attempts -= 1
 
@@ -44,19 +45,21 @@ def guess_length(length):
     return 0    
 
 def randomword():
-    words = ["rainbow", "apple", "orange", "administ",'rainbow', 'computer', 'science', 'programming',
-         'python', 'mathematics', 'player', 'condition',
-         'reverse', 'water', 'board', 'geeks']
+    words = ["rainbow", "apple", "orange", "administ",'rainbow',
+            'computer', 'science', 'programming',
+            'python', 'mathematics', 'player', 'condition',
+            'reverse', 'water', 'board', 'geeks']
     return random.choice(words)
 
 def main():
     print("hello wellcome to guess the word game!\n")
     want2paly = "Y"
-    while want2paly == "Y":
+    while want2paly.upper() == "Y":
         print("im thinking of a word ....")
         word = randomword()
         if guess_length(len(word)):
             gusscharecter(len(word),word)
-
+        want2paly = input("do you want to play again : Y/N ")
+    #say it was fun palying with you if thay dont want to play anymore
 if __name__ == "__main__":
     main()
