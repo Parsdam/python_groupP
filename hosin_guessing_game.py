a7aec289a2eb9efe2ac2f400b1b28583e03314a0
Parsdam <cout.name.hosin01001@gmail.com>
2024-10-09 09:05:10 +0330

    cahnged the name of the file and add some error control

--- hosin_guessing_game.py
+++ hosin_guessing_game.py
@@ -5,9 +5,10 @@ def playgame(min_number, max_number, max_tries):
     number_to_guess = random.randint(min_number, max_number)
     print(f"I'm thinking of a number between {min_number} and {max_number}. You have {max_tries} tries.")
     
-    for attempt in range(max_tries - 1):
+    for attempt in range(max_tries):
         try:
             guess = int(input("whats youre guess ? "))
+            print(f"you have {max_tries - attempt - 1}  attemts remaining")
         except ValueError:
             print("Invalid input. Please enter a number.")
             attempt -= 1
@@ -38,28 +39,31 @@ def main():
             print("Invalid difficulty level selected. Please enter a number.")
             continue
 
-    match (difficulty_lvl):
-        case (1):
+    match difficulty_lvl:
+        case 1:
             print("Easy mode selected")
             playgame(1,10,2)
             pass
-        case (2):
+        case 2:
             print("Medium mode selected")
             playgame(1,100,5)
             pass
-        case (3):
+        case 3:
             print("Hard mode selected")
             playgame(1,1000,10)
             pass
-        case (4):
+        case 4:
             print("custom mod selected")
             try:
-                a=int(input("\nenter minimum number: ")),b=int(input("\nenter maximum number: ")),c=int(input("\nenter tries number: "))
+                a=int(input("\nenter minimum number: "))
+                b=int(input("\nenter maximum number: "))
+                c=int(input("\nenter tries number: "))
             except ValueError:
                 print("Invalid difficulty level selected. Please enter a number")
             playgame(a,b,c)
             pass
         case _:
             print("Invalid difficulty level selected")
+
 if __name__ == "__main__":
     main()
