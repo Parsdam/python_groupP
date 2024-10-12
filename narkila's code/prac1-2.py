d41ababbd5bda61e438e4fe955c610c03c3ab42e
Parsdam <cout.name.hosin01001@gmail.com>
2024-10-12 16:56:05 +0330

    add some notes for improve ment

--- narkila's code/prac1-2.py
+++ narkila's code/prac1-2.py
@@ -9,10 +9,10 @@ while temp_number > 0:
 print(f'sum of dijits is: {sum_dijits}')
 
 is_prime = True
-if num < 2:
-    is_prime = False
+if num < 2:#2 is a prime number so it sould be num <= 2
+    is_prime = False # 1 and 2 are prime numbers so it should be true
 else:
-    for i in range(2, int(num+1)):
+    for i in range(2, int(num+1)):#num is int already y did u type cast it , and u can skip even numbers cuz thay are all non prime
         if num % i == 0:
             is_prime = False
             break
